79ef40503f3e6a120a9fb216cb9994dd134549ad
9916120018 <chhabra.ashish72@gmail.com>
2022-02-15 18:20:48 +0530

    First Project commit

---
+++ Test Suites/GmailTesting.ts
@@ -0,0 +1,35 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>GmailTesting</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>cbc1fbd3-ab8c-455b-b160-0396c014bc20</testSuiteGuid>
+   <testCaseLink>
+      <guid>07856efb-3f11-49ed-94bb-93b91475a5c0</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/GmailLogin</testCaseId>
+      <testDataLink>
+         <combinationType>ONE</combinationType>
+         <id>e022e102-89f0-45d1-8f28-96d314ebcde6</id>
+         <iterationEntity>
+            <iterationType>ALL</iterationType>
+            <value></value>
+         </iterationEntity>
+         <testDataId>Data Files/Gmail_Test data</testDataId>
+      </testDataLink>
+      <variableLink>
+         <testDataLinkId>e022e102-89f0-45d1-8f28-96d314ebcde6</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>email</value>
+         <variableId>9e2c1dd2-1d7d-4e2a-b8e9-aef2340da9c8</variableId>
+      </variableLink>
+   </testCaseLink>
+</TestSuiteEntity>
